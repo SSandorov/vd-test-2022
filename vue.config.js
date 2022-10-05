4419e8b350c186ed05a1330ef45f42e9a4c805b7
ssandorov <49904591+SSandorov@users.noreply.github.com>
2022-10-05 13:16:40 +0200

    diploy test

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/vd-test-2022/' : '/',
   lintOnSave: true,
+  publicPath: process.env.NODE_ENV === 'production' ? '/vd-test-2022/' : '/docs',
 };
